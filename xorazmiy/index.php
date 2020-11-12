5667207bb6315248eeacb3ff5d0abb5d49a95f1b
Ogabek03 <ogabeka03@gmail.com>
2020-11-11 20:27:01 -0500

    I am pulling

--- xorazmiy/index.php
+++ xorazmiy/index.php
@@ -1,123 +1,106 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-		  integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	<title>Document</title>
-	<style>
-        body {
-            width: 100%;
-            height: 700px;
-            background-color: darkgray;
-        }
-
-        .my_from {
-            width: 100%;
-            border-top: 5px solid #e1c089;
-            border-bottom: 5px solid #daba89;
-            height: auto;
-            text-align: center;
-            background-color: antiquewhite;
-        }
-
-        .box {
-            width: 80%;
-            height: 100%;
-            margin: auto;
-            text-align: center !important;
-        }
-
-        .my_table {
-            width: 100%;
-            border-top: 5px solid #e1c089;
-            border-bottom: 5px solid #daba89;
-            height: auto;
-            text-align: center;
-            background-color: antiquewhite;
-        }
-
-        .box table {
-            width: 60%;
-            height: auto;
-            margin: auto;
-        }
-
-        .box table td {
-            text-align: left;
-        }
-
-        .box table tr {
-            text-align: right;
-        }
-
-        .button {
-            text-align: center !important;
-        }
-	</style>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<<<<<<< HEAD
+    <title>Our Blogs</title>
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+    <script src="js/jquery-3.5.1.js"></script>
 </head>
 <body>
 <?php
-$rows = $_GET['rows'] ?? 5;
-$cols = $_GET['cols'] ?? 5;
-$color = $_GET['color'] ?? '#fff';
+include_once "database.php";
+$db = new \Database\database('myitedu');
+$sql =  "SELECT * FROM blogs order by id desc limit 2;";
+$blogs = $db->sql($sql);
 ?>
 <div class="container-fluid">
-	<div class="my_from">
-		<div class="box">
-			<form>
-				<table class="table table-bordered">
-					<tr>
-						<th><label for="exampleInputPassword1">Rows</label></th>
-						<td><input min="0" max="100" type="number" value="<? echo $rows; ?>" name="rows" required="required"></td>
-					</tr>
-					<tr>
-						<th><label for="exampleInputEmail1">Cols</label></th>
-						<td><input min="0" max="100" type="number" value="<? echo $cols; ?>" name="cols" required="required"></td>
-					</tr>
-					<tr>
-						<th><label for="exampleInputPassword1">is Even</label></th>
-						<td><input type="checkbox"></td>
-					</tr>
-					<tr>
-						<th><label for="exampleInputPassword1">Color</label></th>
-						<td><input type="color" name="color" value="<?php echo $color; ?>"></td>
-					</tr>
-					<tr>
-						<td class="button" colspan="2">
-							<button type="submit" class="btn btn-primary">GENERATE</button>
-						</td>
-					</tr>
-				</table>
-			</form>
-		</div>
-	</div>
-	<hr>
-	<div class="my_table">
-		<table class="table table-bordered">
-				<?php
-				$number = 0;
-				for ($row = 0; $row < $rows; $row ++) {
-						echo "<tr>";
-						for ($col = 0; $col < $cols; $col ++) {
-								$number ++;
-								if ($number % 2 === 0) {
-										echo "<td style='background-color:$color'>$number</td>";
-								} else {
-										echo "<td>$number</td>";
-								}
-						}
-						echo "</tr>";
-				}
-				?>
-		</table>
-	</div>
+    <h2>Our Blogs</h2>
+    <div id="blogs">
+
+        <form action="backend.php" method="post">
+            <table class="table table-bordered">
+                <tr>
+                    <td>Subject:</td>
+                    <td><input name="subject" type="text" placeholder="Your new subject"></td>
+                </tr>
+                <tr>
+                    <td>Created By:</td>
+                    <td><select name="created_by">
+                            <option value="1">1</option>
+                            <option selected value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select></td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <button type="reset">Reset</button>
+                        <button type="submit">Submit</button>
+                    </td>
+                </tr>
+            </table>
+        </form>
+
+
+        <div class="accordion" id="accordionExample">
+
+            <?php
+            foreach ($blogs as $row=>$blog){
+            ?>
+            <div class="card">
+                <div class="card-header" id="heading<?php echo $row;?>">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse<?php echo $row;?>" aria-expanded="true" aria-controls="collapse<?php echo $row;?>">
+                            <?php echo $blog['id']; ?>)    <?php echo $blog['subject']; ?>
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapse<?php echo $row;?>" class="collapse show" aria-labelledby="heading<?php echo $row;?>" data-parent="#accordionExample">
+                    <div class="card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                </div>
+            </div>
+
+            <?php
+            }
+            ?>
+
+        </div>
+
+    </div>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-		crossorigin="anonymous"></script>
+<script src="bootstrap/js/bootstrap.js"></script>
 </body>
 </html>
+=======
+    <title>Document</title>
+</head>
+<body>
+<?php
+date_default_timezone_set("America/New_York");
+$mydate = $_GET['datetime']??date('Y-m-d');
+$oneday = 24 * 3600;
+$onemonth = $oneday * 30;
+$oneyear = $onemonth * 12;
+
+$total_year =  time() / $oneyear;
+
+echo time()." = ".round($total_year)." years";
+
+?>
+
+<form>
+    <p>
+        <input type="date" name="datetime" value="<?=$mydate?>" placeholder="Enter Your DOB">
+        <button type="submit">GO</button>
+    </p>
+</form>
+</body>
+</html>
+
+>>>>>>> dd8690d95a59c87b72c5cca79ed0981633e092af
